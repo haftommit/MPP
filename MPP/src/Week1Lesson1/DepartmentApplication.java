6c76b5782886d8dfb25029f70152cfa47f8f1975
Haftom Alemayehu <haftommit@gmail.com>
2016-01-11 16:44:26 -0600

    Mpp first commit

---
+++ MPP/src/Week1Lesson1/DepartmentApplication.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+package Week1Lesson1;
+
+/**
+ * @author haftomtesfay
+ *
+ */
+public class DepartmentApplication {
+
+	/**
+	 * 
+	 */
+	public DepartmentApplication() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ MPP/src/Week1Lesson1/package-info.java
@@ -0,0 +1,8 @@
+
+/**
+ * @author haftomtesfay
+ *
+ *Write a program that is used by the computer science department to keep track of students, 
+ *faculty and staff within the department, the courses students take, and the courses faculty teach.
+ */
+package Week1Lesson1;
