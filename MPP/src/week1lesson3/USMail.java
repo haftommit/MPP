1320a8e6c58ea4f9b552055b0917f844ef889f6f
Haftom Alemayehu <haftommit@gmail.com>
2016-01-19 08:13:08 -0600

    Ok

--- MPP/src/week1lesson3/USMail.java
+++ MPP/src/week1lesson3/USMail.java
@@ -4,14 +4,14 @@ package week1lesson3;
  * @author haftomtesfay
  *
  */
-public class USMail extends Package {
+public class USMail extends Carrier {
 
 	public USMail(String desc, double weight, String zone, String sender) {
 		super(desc, weight, zone, sender);
 		// TODO Auto-generated constructor stub
 	}
 
-	public double cost() {
+	public double calculateRate() {
 		if (weight <= 3)
 			return 1;
 		else
