047b9606d89d6a20c65d6df2f12b3d758ef3805b
Haftom Alemayehu <haftommit@gmail.com>
2016-02-01 18:05:39 -0600

    updates

--- MPP/src/javaFXLab/package-info.java
+++
@@ -1,8 +0,0 @@
-/**
- * 
- */
-/**
- * @author haftomtesfay
- *
- */
-package javaFXLab;
--- MPP/src/lamdaexpressions/ArraysAndStreams.java
+++ MPP/src/lamdaexpressions/ArraysAndStreams.java
@@ -1,6 +1,6 @@
  package lamdaexpressions;
 
-import java.awt.List;
+import java.util.List;
 import java.util.Arrays;
 import java.util.stream.Collectors;
 
